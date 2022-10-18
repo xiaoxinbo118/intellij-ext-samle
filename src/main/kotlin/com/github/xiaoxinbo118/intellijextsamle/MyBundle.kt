e09a454c248b98c96e1a21ac79a7bf18688d0c83
GitHub Action <action@github.com>
2022-10-18 03:44:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiaoxinbo118/intellijextsamle/MyBundle.kt
+++ src/main/kotlin/com/github/xiaoxinbo118/intellijextsamle/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.xiaoxinbo118.intellijextsamle
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/xiaoxinbo118/intellijextsamle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiaoxinbo118/intellijextsamle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiaoxinbo118.intellijextsamle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiaoxinbo118.intellijextsamle.MyBundle
 
 class MyApplicationService {
 
